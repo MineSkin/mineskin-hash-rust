3cf8f918061d36975acc7ab7336550f6e7d1045c
Haylee Schäfer <haylee@inventivetalent.org>
2024-09-19 16:34:11 +0200

    add test for trimmed 0

--- __test__/index.spec.mjs
+++ __test__/index.spec.mjs
@@ -9,7 +9,7 @@ test('sum from native', (t) => {
 
 // empty hash a67c89456ea2fcd58cdf557c01357632d4f0d28c51dd0d8f79a4c739352f71a7
 
-function testEncode(t, imagePath, expectedHash) {
+function testEncode(t, imagePath, expectedHash, trim = false) {
     let buffer = fs.readFileSync(imagePath);
 
     //let arr = new Uint8Array(buffer);
@@ -19,6 +19,9 @@ function testEncode(t, imagePath, expectedHash) {
     console.log('hash    ', hash);
 
     let mchash = Buffer.from(encoded.minecraftHash).toString('hex');
+    if (trim) {
+        mchash = mchash.replace(/^0+/, '');
+    }
     console.log('mchash  ', mchash);
 
     console.log('expected', expectedHash);
@@ -35,7 +38,8 @@ for (const params of [
     ['images/randomImage2.png', '57a6d6ddebafe045e9a0501c7c489f6de47e670fc4ef1b0c1b2c2a9a2c8c0cbc'],
     ['images/randomImage3.png', '1b84a8081994cce186eeae4787f95bce663d6711272782ce659d9cc954fec435'],
     ['images/randomImage4.png', 'e19abca2800650b449fff3f12ed3d1bd0f20861d7cdd00c66d62264b52837e2c'],
-    ['images/trimmed0.png', '0aa97a60ff386b6075aea693893106358def0cd580e6360599811e08bde38b6f'] // mojang strips the leading 0, this doesn't currently
+    ['images/trimmed0.png', '0aa97a60ff386b6075aea693893106358def0cd580e6360599811e08bde38b6f'], // mojang strips the leading 0, this doesn't currently
+    ['images/trimmed0.png', 'aa97a60ff386b6075aea693893106358def0cd580e6360599811e08bde38b6f', true]
 ]) {
     test(testEncode, ...params);
 }
